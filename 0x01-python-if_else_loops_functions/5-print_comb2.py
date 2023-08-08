ae9d9a04b4fb88ce7fec78b353de39185060a388
faty <fatyboubakri31@gmail.com>
2023-08-08 16:52:23 +0000

    one loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 for i in range(0, 10):
-    for j in range(0, 10):
+    j = 0
+    while (j < 10):
         if (i == 9 and j == 9):
             print("99")
         else:
             print("{}{}".format(i, j), end=", ")
+        j += 1
