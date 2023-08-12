7ba1c04c7468dc64e75352dac28d194f7c2efdab
faty <fatyboubakri31@gmail.com>
2023-08-12 18:13:51 +0000

    Print matrix integer

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(0, len(row)):
+            print("{:d} ".format(row[i]), end="")
+        print()
