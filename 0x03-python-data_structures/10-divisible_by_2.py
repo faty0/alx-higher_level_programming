5ab08e9b02dc71d2a6132a25742c85820eb611c4
faty <fatyboubakri31@gmail.com>
2023-08-12 19:25:03 +0000

    Divisible by two

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    tof = []
+    for i in my_list:
+        if i % 2 == 0:
+            tof.append(True)
+        else:
+            tof.append(False)
+    return tof
