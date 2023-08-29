f5b42a14e6131674d9a0895853b15a8903ef925c
faty <fatyboubakri31@gmail.com>
2023-08-29 21:21:36 +0000

    Add doscstring to the module task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python3
+"""
+This module provides a Square class that defines a square.
+
+Classes:
+    Square
+
+Functions:
+    None
+
+Variables:
+    None
+"""
+
+
 class Square:
-    """an empty class Square that defines a square"""
+    """
+    an empty class Square that defines a square
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
