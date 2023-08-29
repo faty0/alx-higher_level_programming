c17a9acef8190e7b6eab607406087701eb66e361
faty <fatyboubakri31@gmail.com>
2023-08-29 22:21:55 +0000

    Instantiation with optional

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module provides a Square class that defines a square.
+
+Classes:
+    Square
+
+Functions:
+    None
+
+Variables:
+    None
+"""
+
+
+class Square:
+    """
+    an empty class Square that defines a square
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__: Initializes a Square object with a given size parameter
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
