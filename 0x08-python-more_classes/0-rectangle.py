d4b440ceceb8bd235edf69cce828923281eb1d40
faty <fatyboubakri31@gmail.com>
2023-09-04 14:31:03 +0000

    Add task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    This module provides a class to represent a rectangle
+
+    Classes:
+        Rectangle: Class defining a rectangle
+
+'''
+
+
+class Rectangle:
+    '''
+    Class defining a rectangle
+    '''
+
+    pass
