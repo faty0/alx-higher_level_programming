61e892994913b73b6c609c712a5b18ac03444a65
faty <fatyboubakri31@gmail.com>
2023-09-04 15:15:48 +0000

    Task 2 update

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -77,4 +77,6 @@ class Rectangle:
         '''
         Calculate the rectangle perimeter
         '''
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (2 * (self.__width + self.__height))
