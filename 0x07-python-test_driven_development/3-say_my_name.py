ee008b090f8344c4cfc27a0718cea570c7f30bab
faty <fatyboubakri31@gmail.com>
2023-09-08 19:58:55 +0000

    Add task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+''' a module for printing the full name
+'''
+
+
+def say_my_name(first_name, last_name=""):
+    ''' a function that prints My name is <first name> <last name>
+    '''
+    if (not isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    elif (not isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
