36691543cdbe9f3865d588d1189afea858534504
faty <fatyboubakri31@gmail.com>
2023-09-10 18:38:44 +0000

    Add 1 element case task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@ max_integr = __import__("6-max_integer").max_integer
 class TestMaxInteger(unittest.TestCase):
 
     def test_max_integer(self):
+        self.assertEqual(max_integr([4]), 4)
         self.assertEqual(max_integr([1, 2, 3, 4]), 4)
         self.assertEqual(max_integr([4, 3, 2, 1]), 4)
         self.assertEqual(max_integr([0, 3, 4, 2, 1]), 4)
