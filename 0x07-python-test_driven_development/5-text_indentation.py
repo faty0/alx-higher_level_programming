ede54fec7f9ca3280c5802d69ee113edef628396
faty <fatyboubakri31@gmail.com>
2023-09-10 15:40:54 +0000

    Fix all checks task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,10 +7,13 @@ def text_indentation(text):
     ''' a function that prints a text with 2 new lines
     after each of these characters: ., ? and :
     '''
+    if (not isinstance(text, str)):
+        raise TypeError("text must be a string")
+
     marks = [".", "?", ":"]
     for i in range(0, len(text)):
-        print(text[i], end="")
-        if (i > 0):
-            if (text[i - 1] in marks):
-                print()
-                print()
+        if ((i > 0) and (text[i - 1] in marks)):
+            print()
+            print()
+        else:
+            print(text[i], end="")
