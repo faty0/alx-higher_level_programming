94b7c7ee41c2111d6281f1f5a36fc1563ebe8ceb
faty <fatyboubakri31@gmail.com>
2023-09-12 14:29:10 +0000

    Print with end=

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     ''' read a text file (UTF8) and prints it to stdout
     '''
     with open(filename, 'r', encoding="utf-8") as f:
-        reads = f.read()
-    print(reads)
+        print(f.read(), end="")
