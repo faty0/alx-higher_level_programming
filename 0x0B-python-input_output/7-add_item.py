b18925518a90ca888ffe75450f3a7ccbf84c85f8
faty <fatyboubakri31@gmail.com>
2023-09-12 18:21:51 +0000

    Handle FileNotFoundError

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,11 +11,10 @@ from os import path, mknod
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_list = []
-if path.exists("add_item.json"):
+try:
     my_list = load_from_json_file("add_item.json")
-    for i in range(1, len(argv)):
-        my_list.append(argv[i])
-    save_to_json_file(my_list, "add_item.json")
-else:
-    save_to_json_file(my_list, "add_item.json")
+except FileNotFoundError:
+    my_list = []
+for i in range(1, len(argv)):
+    my_list.append(argv[i])
+save_to_json_file(my_list, "add_item.json")
