a297e145def5d4b3beced50590f8097b838940e4
faty <fatyboubakri31@gmail.com>
2023-09-12 17:43:09 +0000

    Add task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,18 +5,18 @@ and then save them to a file
 
 
 from sys import argv
+from os import path, mknod
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 my_list = []
-if len(argv) > 1:
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except Exception as e:
-        with open("add_item.json", 'w', encoding="utf_8")
-    for arg in argv:
-        my_list.append(arg)
+if path.exists("add_item.json"):
+    my_list = load_from_json_file("add_item.json")
+    for i in range(1, len(argv)):
+        my_list.append(argv[i])
     save_to_json_file(my_list, "add_item.json")
-
+else:
+    mknod("add_item.json")
+    save_to_json_file(my_list, "add_item.json")
