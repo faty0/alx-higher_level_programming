23e0f2a313bf1eec5a57f9a63071c56b6f49d141
faty <fatyboubakri31@gmail.com>
2023-09-12 21:30:45 +0000

    Add case empty dict task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -23,6 +23,7 @@ class Student:
         return dict
 
     def reload_from_json(self, json):
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if json:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
