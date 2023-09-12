7edd72d1f8abbcae1ea126b83adc1a4eb30aac04
faty <fatyboubakri31@gmail.com>
2023-09-12 14:26:51 +0000

    Fixing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,5 @@ def read_file(filename=""):
     ''' read a text file (UTF8) and prints it to stdout
     '''
     with open(filename, 'r', encoding="utf-8") as f:
-        print(f.read())
+        reads = f.read()
+    print(reads)
