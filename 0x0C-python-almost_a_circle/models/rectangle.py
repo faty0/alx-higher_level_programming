321e6b68843000dfa2ecc9923037a585d69785de
faty <fatyboubakri31@gmail.com>
2023-09-19 21:20:13 +0000

    Add task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,32 +15,48 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        ''' getter for the width attribute
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
+        ''' setter for the width attribute
+        '''
         self.__width = value
 
     @property
     def height(self):
+        ''' getter for the height attribute
+        '''
         return self.__height
 
     @height.setter
     def height(self, value):
+        ''' setter for the height attribute
+        '''
         self.__height = value
 
     @property
     def x(self):
+        ''' getter for the x attribute
+        '''
         return self.__x
 
     @x.setter
     def x(self, value):
+        ''' setter for the x attribute
+        '''
         self.__x = value
 
     @property
     def y(self):
+        ''' getter for the y attribute
+        '''
         return self.__y
 
     @y.setter
     def y(self, value):
+        ''' setter for the y attribute
+        '''
         self.__y = value
