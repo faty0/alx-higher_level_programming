c4cbec861f053f97a83151f471376bfa4f331377
faty <fatyboubakri31@gmail.com>
2023-11-17 10:10:50 +0000

    show table status like T-n

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
 -- prints the full description
--- of the table first_table from the database hbtn_0c_0
-DESCRIBE first_table;
+-- of the table first_table from the database hbtn_0c_0-- Check current character set and collation for the table
+SHOW TABLE STATUS LIKE 'first_table';
