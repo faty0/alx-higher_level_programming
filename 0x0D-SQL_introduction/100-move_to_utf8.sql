98900e5f9d0cce9e135f900f963a64a7f73a6a98
faty <fatyboubakri31@gmail.com>
2023-11-17 10:06:48 +0000

    backtic to task 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,17 @@
 -- Convert the database to UTF8
-ALTER DATABASE hbtn_0c_0
+ALTER DATABASE `hbtn_0c_0`
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
-USE hbtn_0c_0;
+USE `hbtn_0c_0`;
 
 -- Convert the table to UTF8
-ALTER TABLE first_table
+ALTER TABLE `first_table`
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the field to UTF8
-ALTER TABLE first_table
+ALTER TABLE `first_table`
 MODIFY name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
