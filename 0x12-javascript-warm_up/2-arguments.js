997d490aa87eeaa20d70f11b064f9a3a50f2db64
faty <fatyboubakri31@gmail.com>
2023-12-11 21:57:35 +0000

    task 2 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-const argvCount = Object.keys(argv).length;
-if (argvCount === 2) {
+let argvCount = 0;
+
+argv.forEach((val, index) => {
+    argvCount++;
+  });
+
+if (argvCount === 0) {
   console.log('No argument');
-} else if (argvCount === 3) {
+} else if (argvCount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
