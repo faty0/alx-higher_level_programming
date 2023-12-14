3038a7fd9bbf5db4ab4e8cbec8d5e2b306aae8a6
faty <fatyboubakri31@gmail.com>
2023-12-14 18:17:43 +0000

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let display = '';
+    for (let j = 0; j < this.width; j++) {
+      display += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(display);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
