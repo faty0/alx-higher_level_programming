ba725c16aa4b2d59a81472f9785786552e5e0c94
faty <fatyboubakri31@gmail.com>
2023-12-14 17:55:27 +0000

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-  
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
+  print () {
+    let display = '';
+    for (let j = 0; j < this.width; j++) {
+      display += 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(display);
+    }
+  }
 }
 module.exports = Rectangle;
-  
